a4ea2c2e3f7fa0a3d33917a3243b6301fef18180
n-kall <noa@tutamail.com>
2019-02-23 23:23:59 +0100

    horizontal graphs

--- online_graphs.R
+++ online_graphs.R
@@ -9,13 +9,13 @@ motorist_names <- c("self-driving" = "Self-driving car",
 
 a <- emmip(carped_cov_fac_glmm, ~ ratio_f | perspective | motorist,
            type = "response") +
-  facet_grid(perspective ~ motorist, labeller = as_labeller(motorist_names)) +
+  facet_grid(motorist ~ perspective, labeller = as_labeller(motorist_names)) +
   xlab("Lives-at-risk") +
   ylab("Probability of preferring swerve") +
   ylim(0, 1) + 
   geom_bar(stat="identity", aes(fill=perspective), color = "black") +
   geom_line(aes(group=perspective)) +
-  geom_point(size = 0.5) +
+  geom_point(size = 1) +
   scale_fill_manual(name = "Perspective",
                     values = c("#E41A1C",
                                "#377EB8",
@@ -23,7 +23,7 @@ a <- emmip(carped_cov_fac_glmm, ~ ratio_f | perspective | motorist,
                     labels = c("Car occupant",
                                "Observer",
                                "Pedestrian")) +
-  theme(strip.text.y = element_blank(),
+  theme(strip.text.x = element_blank(),
         panel.grid = element_blank(),
         axis.text = element_text(size = 8),
         axis.title = element_text(size = 12)) 
@@ -31,18 +31,18 @@ a <- emmip(carped_cov_fac_glmm, ~ ratio_f | perspective | motorist,
 a$layers[[1]] <- NULL
 a
 
-ggsave("online_carped.pdf", plot = a, width = 7, height = 7)
+ggsave("online_carped.pdf", plot = a, width = 7, height = 4)
 
 
 b <- emmip(pedped_cov_fac_glmm, ~ ratio_f | perspective | motorist,
       type = "response") +
-  facet_grid(perspective ~ motorist, labeller = as_labeller(motorist_names)) +
+  facet_grid(motorist ~ perspective, labeller = as_labeller(motorist_names)) +
   xlab("Lives-at-risk") +
   ylab("Probability of preferring swerve") +
   ylim(0, 1) + 
   geom_bar(stat="identity", aes(fill=perspective), color = "black") +
   geom_line(aes(group=perspective)) +
-  geom_point(size = 0.5) +
+  geom_point(size = 1) +
   scale_fill_manual(name = "Perspective",
                     values = c("#E41A1C",
                                "#377EB8",
@@ -52,7 +52,7 @@ b <- emmip(pedped_cov_fac_glmm, ~ ratio_f | perspective | motorist,
                                "Observer",
                                "Pedestrian (forward)",
                                "Pedestrian (side)")) +
-  theme(strip.text.y = element_blank(),
+  theme(strip.text.x = element_blank(),
         panel.grid = element_blank(),
         axis.text = element_text(size = 8),
         axis.title = element_text(size = 12)) 
@@ -60,6 +60,6 @@ b <- emmip(pedped_cov_fac_glmm, ~ ratio_f | perspective | motorist,
 b$layers[[1]] <- NULL
 b
 
-ggsave("online_pedped.pdf", plot = b, height = 7,
-       width = 7)
+ggsave("online_pedped.pdf", plot = b, height = 4,
+       width = 10)
 
