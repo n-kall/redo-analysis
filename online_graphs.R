6b616a513a6acc5301774a238ca036dedb2f32a7
n-kall <noa@tutamail.com>
2019-02-23 21:52:58 +0100

    add new graphs for online study

---
+++ online_graphs.R
@@ -0,0 +1,52 @@
+library(emmeans)
+library(tidyverse)
+
+load("online_study_redo2.RData")
+
+motorist_names <- c("self-driving" = "Self-driving car",
+                    "human" = "Human driver")
+
+a <- emmip(carped_cov_fac_glmm, ~ ratio_f | perspective | motorist,
+           type = "response") +
+  facet_grid(perspective ~ motorist, labeller = as_labeller(motorist_names)) +
+  xlab("Lives-at-risk") +
+  ylab("Probability of preferring swerve") +
+  ylim(0, 1) + geom_bar(stat="identity", aes(fill=perspective)) +
+  geom_line(aes(group=perspective)) +
+  geom_point(size = 0.5) +
+  scale_fill_brewer(name = "Perspective",
+                    palette = 6, type = "qual",
+                    labels = c("Car occupant",
+                               "Observer",
+                               "Pedestrian (forward)",
+                               "Pedestrian (side)")) +
+  theme(strip.text.y = element_blank(),
+        panel.grid = element_blank())
+
+a$layers[[1]] <- NULL
+a
+
+ggsave("online_carped.pdf", plot = a)
+
+
+b <- emmip(pedped_cov_fac_glmm, ~ ratio_f | perspective | motorist,
+      type = "response") +
+  facet_grid(perspective ~ motorist, labeller = as_labeller(motorist_names)) +
+  xlab("Lives-at-risk") +
+  ylab("Probability of preferring swerve") +
+  ylim(0, 1) + geom_bar(stat="identity", aes(fill=perspective)) +
+  geom_line(aes(group=perspective)) +
+  geom_point(size = 0.5) +
+  scale_fill_brewer(name = "Perspective",
+                    palette = 6, type = "qual",
+                    labels = c("Car occupant",
+                               "Observer",
+                               "Pedestrian (forward)",
+                               "Pedestrian (side)")) +
+  theme(strip.text.y = element_blank(),
+        panel.grid = element_blank())
+
+b$layers[[1]] <- NULL
+b
+
+ggsave("online_pedped.pdf", plot = b)
